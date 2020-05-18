f1408bc0d2fc19608d3e6777604322bebc88d2ce
dinarazek <44913703+dinarazek@users.noreply.github.com>
2020-05-18 14:20:03 -0500

    Update and rename build.gradle to build.gradle.kts

---
+++ mobile-simulation/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+   id("us.ihmc.ihmc-build") version "0.20.1"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "0.0"
+   vcsUrl = "https://github.com/ihmcrobotics/ihmc-open-robotics-software-tutorials"
+   openSource = true
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+mainDependencies {
+   api("us.ihmc:simulation-construction-set:0.12.0")
+}
