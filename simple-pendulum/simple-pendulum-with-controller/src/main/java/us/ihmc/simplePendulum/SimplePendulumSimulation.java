3dfdd33a94d7e26f30d27795e7eb2b0e6c5ad223
Dina Razek <drazek@andrew.cmu.edu>
2020-06-24 11:12:11 -0500

    Make formatting changes; fix Javadoc issues

--- simple-pendulum/simple-pendulum-with-controller/src/main/java/us/ihmc/simplePendulum/SimplePendulumSimulation.java
+++ simple-pendulum/simple-pendulum-with-controller/src/main/java/us/ihmc/simplePendulum/SimplePendulumSimulation.java
@@ -3,54 +3,52 @@ package us.ihmc.simplePendulum;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
 
-public class SimplePendulumSimulation {
+public class SimplePendulumSimulation
+{
 
-	// simulation step duration
-	public static final double DT = 0.001;
+   // simulation step duration
+   public static final double DT = 0.001;
 
-	private SimulationConstructionSet sim;
+   private SimulationConstructionSet sim;
 
-	public SimplePendulumSimulation() {
+   public SimplePendulumSimulation()
+   {
 
-		SimplePendulumRobot robot = new SimplePendulumRobot();
-		robot.setController(new SimplePendulumController(robot));
+      SimplePendulumRobot robot = new SimplePendulumRobot();
+      robot.setController(new SimplePendulumController(robot));
 
-		/* Creates simulation parameters */
-		SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters();
-		/*
-		 * Sets data buffer to allow for this number of values for each variable to be
-		 * saved.
-		 */
-		parameters.setDataBufferSize(32000);
+      /* Creates simulation parameters */
+      SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters();
+      // Sets data buffer to allow for this number of values for each variable to be saved.
+      parameters.setDataBufferSize(32000);
+      // Creates a new simulation
+      sim = new SimulationConstructionSet(robot, parameters);
 
-		// Creates a new simulation
-		sim = new SimulationConstructionSet(robot, parameters);
+      /*
+       * Sets the simulation to collect data every 20 simulation steps This is used to prune data so a
+       * smaller buffer is sufficient.
+       */
+      sim.setDT(DT, 20);
 
-		/*
-		 * Sets the simulation to collect data every 20 simulation steps This is used to
-		 * prune data so a smaller buffer is sufficient.
-		 */
-		sim.setDT(DT, 20);
+      // Sets the ground to be visbile in the 3D view
+      sim.setGroundVisible(true);
 
-		// Sets the ground to be visbile in the 3D view
-		sim.setGroundVisible(true);
+      // Sets location and orientation of the camera
+      sim.setCameraPosition(0, -9.0, 0.6);
+      sim.setCameraFix(0.0, 0.0, 0.70);
 
-		// Sets location and orientation of the camera
-		sim.setCameraPosition(0, -9.0, 0.6);
-		sim.setCameraFix(0.0, 0.0, 0.70);
+      /*
+       * Specifies that the simulation will only run for a duration of 60 seconds. For this tutorial, this
+       * allows the simulation to run to a point where it doesn't overflow the data buffer
+       */
+      sim.setSimulateDuration(60.0);
 
-		/*
-		 * Specifies that the simulation will only run for a duration of 60 seconds. For
-		 * this tutorial, this allows the simulation to run to a point where it doesn't
-		 * overflow the data buffer
-		 */
-		sim.setSimulateDuration(60.0);
+      // Launch the simulator.
+      sim.startOnAThread();
+   }
 
-		// Launch the simulator.
-		sim.startOnAThread();
-	}
-
-	public static void main(String[] args) {
-		new SimplePendulumSimulation();
-	}
+   public static void main(String[] args)
+   {
+      new SimplePendulumSimulation();
+   }
 }
