9ff92dd1943729fdb27c74d16a020d89ad41f8e2
Dina Razek <drazek@andrew.cmu.edu>
2020-06-25 14:47:46 -0500

    Add falling brick project

---
+++ simple-pendulum/falling-brick/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+   id("us.ihmc.ihmc-build") version "0.20.2"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "0.0"
+   vcsUrl = "https://github.com/ihmcrobotics/ihmc-open-robotics-software-tutorials"
+   openSource = true
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+mainDependencies {
+   api("us.ihmc:simulation-construction-set:0.16.0")
+   api("us.ihmc:simulation-construction-set-tools:0.13.0-200518")
+   api("us.ihmc:ihmc-robotics-toolkit:0.13.0-200518")
+}
