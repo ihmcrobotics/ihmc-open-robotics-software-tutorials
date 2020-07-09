d5951cbc94f2bd23b8c027525b084d0c9f15a87c
Dina Razek <drazek@andrew.cmu.edu>
2020-07-09 16:02:00 -0500

    Remove unnecessary main dependencies

--- simulation-construction-set/simple-pendulum-with-controller/build.gradle.kts
+++ simulation-construction-set/simple-pendulum-with-controller/build.gradle.kts
@@ -13,7 +13,4 @@ ihmc {
 }
 
 mainDependencies {
-   api("us.ihmc:simulation-construction-set:0.16.0")
-   api("us.ihmc:simulation-construction-set-tools:0.13.0-200518")
-   api("us.ihmc:ihmc-robotics-toolkit:0.13.0-200518")
 }
