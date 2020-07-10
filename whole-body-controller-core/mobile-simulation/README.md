f98fd3e1145763efbad56cab31ef2fd6db97aad8
Dina Razek <drazek@andrew.cmu.edu>
2020-07-10 11:24:16 -0500

    Reformat files, move mobile-simulation to scs examples

--- whole-body-controller-core/mobile-simulation/README.md
+++
@@ -1,3 +0,0 @@
-# IHMC Robotics: Mobile Simulation
-
-This project is part of the following tutorial set: https://github.com/ihmcrobotics/dynamic-walking-tutorial-2018/wiki/Step-2-The-Mobile-example-simulation
--- whole-body-controller-core/robot-arm-one/src/main/java/us/ihmc/robotArmOne/RobotArmOneSimulation.java
+++ whole-body-controller-core/robot-arm-one/src/main/java/us/ihmc/robotArmOne/RobotArmOneSimulation.java
@@ -3,39 +3,42 @@ package us.ihmc.robotArmOne;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 
 /**
- * In this example simulation, we will create a simple 7-DoF robot arm
- * {@code RobotArmOne} which is controlled with simple PD controllers on each
- * joint to track sine-wave trajectories, see {@code RobotArmOneController}.
+ * In this example simulation, we will create a simple 7-DoF robot arm {@code RobotArmOne} which is
+ * controlled with simple PD controllers on each joint to track sine-wave trajectories, see
+ * {@code RobotArmOneController}.
  * <p>
  * This is the main class that sets up and starts the simulation environment.
  * </p>
  */
-public class RobotArmOneSimulation {
-	public RobotArmOneSimulation() {
-		// Create an instance of the robot arm.
-		RobotArmOne robotArm = new RobotArmOne();
-		// Create an instance of the controller.
-		RobotArmOneController robotArmController = new RobotArmOneController(robotArm);
-		// Make sure to initialize the controller.
-		robotArmController.initialize();
-		// Attach the controller to the robot.
-		robotArm.setController(robotArmController);
+public class RobotArmOneSimulation
+{
+   public RobotArmOneSimulation()
+   {
+      // Create an instance of the robot arm.
+      RobotArmOne robotArm = new RobotArmOne();
+      // Create an instance of the controller.
+      RobotArmOneController robotArmController = new RobotArmOneController(robotArm);
+      // Make sure to initialize the controller.
+      robotArmController.initialize();
+      // Attach the controller to the robot.
+      robotArm.setController(robotArmController);
 
-		// Creating the simulation.
-		SimulationConstructionSet scs = new SimulationConstructionSet(robotArm);
-		// As this example simulation is rather simple, let's prevent SCS from
-		// simulating faster than real-time.
-		scs.setSimulateNoFasterThanRealTime(true);
-		// Defining the simulation DT and the frequency at which data is logged.
-		scs.setDT(1.0e-4, 10);
-		// Defining the buffer size to ensure a minimum simulation duration before
-		// filling the graphs in the simulator.
-		scs.setMaxBufferSize(65536);
-		// Launch the simulator.
-		scs.startOnAThread();
-	}
+      // Creating the simulation.
+      SimulationConstructionSet scs = new SimulationConstructionSet(robotArm);
+      // As this example simulation is rather simple, let's prevent SCS from
+      // simulating faster than real-time.
+      scs.setSimulateNoFasterThanRealTime(true);
+      // Defining the simulation DT and the frequency at which data is logged.
+      scs.setDT(1.0e-4, 10);
+      // Defining the buffer size to ensure a minimum simulation duration before
+      // filling the graphs in the simulator.
+      scs.setMaxBufferSize(65536);
+      // Launch the simulator.
+      scs.startOnAThread();
+   }
 
-	public static void main(String[] args) {
-		new RobotArmOneSimulation();
-	}
+   public static void main(String[] args)
+   {
+      new RobotArmOneSimulation();
+   }
 }
