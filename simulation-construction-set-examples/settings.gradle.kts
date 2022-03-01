758b71d5493a96eb5f1c0b5a57745baff101f859
Sylvain Bertrand <sbertrand@ihmc.org>
2022-03-01 09:18:35 -0600

    Updated the build plugin.

--- simulation-construction-set-examples/settings.gradle.kts
+++ simulation-construction-set-examples/settings.gradle.kts
@@ -1,10 +1,16 @@
+pluginManagement {
+   plugins {
+      id("us.ihmc.ihmc-build") version "0.28.0"
+   }
+}
+
 buildscript {
    repositories {
       maven { url = uri("https://plugins.gradle.org/m2/") }
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:0.25.0")
+      classpath("us.ihmc:ihmc-build:0.28.0")
    }
 }
 
