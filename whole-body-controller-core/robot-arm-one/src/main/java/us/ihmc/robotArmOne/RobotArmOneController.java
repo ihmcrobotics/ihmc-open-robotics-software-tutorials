3d3168e6c54697dff7bdba6f92bc3a269ed552c1
meugster <meugster@10.100.2.46>
2023-02-13 18:40:15 -0600

    Fixed controller input/ output for cleaned-up version of Example 1 in SCS2

--- whole-body-controller-core/robot-arm-one/src/main/java/us/ihmc/robotArmOne/RobotArmOneController.java
+++ whole-body-controller-core/robot-arm-one/src/main/java/us/ihmc/robotArmOne/RobotArmOneController.java
@@ -9,6 +9,7 @@ import us.ihmc.robotArmOne.SevenDoFArmParameters.SevenDoFArmJointEnum;
 import us.ihmc.scs2.definition.controller.ControllerInput;
 import us.ihmc.scs2.definition.controller.ControllerOutput;
 import us.ihmc.scs2.definition.controller.interfaces.Controller;
+import us.ihmc.scs2.definition.state.interfaces.OneDoFJointStateBasics;
 import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoDouble;
 
@@ -18,31 +19,11 @@ public class RobotArmOneController implements Controller
    private final String name = "robotArmController";
 
    private final EnumMap<SevenDoFArmJointEnum, OneDoFJointReadOnly> robotJoints = new EnumMap<>(SevenDoFArmJointEnum.class);
-//   private OneDoFJointReadOnly shoulderYawJoint;
-//   private OneDoFJointReadOnly shoulderRollJoint;
-//   private OneDoFJointReadOnly shoulderPitchJoint;
-//   private OneDoFJointReadOnly elbowPitchJoint;
-//   private OneDoFJointReadOnly wristPitchJoint;
-//   private OneDoFJointReadOnly wristRollJoint;
-//   private OneDoFJointReadOnly wristYawJoint;
-
+   private final EnumMap<SevenDoFArmJointEnum, OneDoFJointStateBasics> robotJointsOutputs = new EnumMap<>(SevenDoFArmJointEnum.class);
+   
    private ControllerInput controllerInput;
 
-   private ControllerOutput controllerOutput;
-
    private static final double TWO_PI = 2.0 * Math.PI;
-
-//   /**
-//    * Current position for each joint. {@code YoDouble}s are used instead of simple {@code double} so
-//    * they can be monitored via the Simulation Construction Set.
-//    */
-//   private final EnumMap<SevenDoFArmJointEnum, YoDouble> currentPositions = new EnumMap<>(SevenDoFArmJointEnum.class);
-//
-//   /**
-//    * Current velocities for each joint. {@code YoDouble}s are used instead of simple {@code double} so
-//    * they can be monitored via the Simulation Construction Set.
-//    */
-//   private final EnumMap<SevenDoFArmJointEnum, YoDouble> currentVelocities = new EnumMap<>(SevenDoFArmJointEnum.class);
 
    /**
     * We use this registry to keep track of the controller variables which can then be viewed in
@@ -82,23 +63,14 @@ public class RobotArmOneController implements Controller
 
    public RobotArmOneController(ControllerInput controllerInput, ControllerOutput controllerOutput)
    {
-
+      
       this.controllerInput = controllerInput;
-      this.controllerOutput = controllerOutput;
-
-      // Get the objects of the fulcrum joint to read input and write output in the control loop
-//      this.shoulderYawJoint = (OneDoFJointReadOnly) controllerInput.getInput().findJoint(SevenDoFArmJointEnum.shoulderYaw.getJointName());
-//      this.shoulderRollJoint = (OneDoFJointReadOnly) controllerInput.getInput().findJoint(SevenDoFArmJointEnum.shoulderRoll.getJointName());
-//      this.shoulderPitchJoint = (OneDoFJointReadOnly) controllerInput.getInput().findJoint(SevenDoFArmJointEnum.shoulderPitch.getJointName());
-//      this.elbowPitchJoint = (OneDoFJointReadOnly) controllerInput.getInput().findJoint(SevenDoFArmJointEnum.elbowPitch.getJointName());
-//      this.wristYawJoint = (OneDoFJointReadOnly) controllerInput.getInput().findJoint(SevenDoFArmJointEnum.wristYaw.getJointName());
-//      this.wristRollJoint = (OneDoFJointReadOnly) controllerInput.getInput().findJoint(SevenDoFArmJointEnum.wristRoll.getJointName());
-//      this.wristPitchJoint = (OneDoFJointReadOnly) controllerInput.getInput().findJoint(SevenDoFArmJointEnum.wristPitch.getJointName());
-
+      
       /*
        * YoDoubles need to be created first with a given name that is to represent the variable in the
        * Simulation Construction Set, and the registry so the simulation can find them.
        */
+     
       for (SevenDoFArmJointEnum jointEnum : SevenDoFArmJointEnum.values())
       {
          String jointName = StringUtils.capitalize(jointEnum.getJointName());
@@ -109,10 +81,9 @@ public class RobotArmOneController implements Controller
 
          positionErrors.put(jointEnum, new YoDouble("positionError" + jointName, registry));
          velocityErrors.put(jointEnum, new YoDouble("velocityError" + jointName, registry));
-         
-         robotJoints.put(jointEnum, (OneDoFJointReadOnly) controllerInput.getInput().findJoint(SevenDoFArmJointEnum.wristPitch.getJointName()));
-//         currentPositions.put(jointEnum, new YoDouble("currentPosition" + jointName, registry));
-//         currentVelocities.put(jointEnum, new YoDouble("currentVelocities" + jointName, registry));
+
+         robotJoints.put(jointEnum, (OneDoFJointReadOnly) controllerInput.getInput().findJoint(jointEnum.getJointName()));
+         robotJointsOutputs.put(jointEnum, (OneDoFJointStateBasics) controllerOutput.getOneDoFJointOutput(jointEnum.getJointName()));       
       }
    }
 
@@ -174,29 +145,13 @@ public class RobotArmOneController implements Controller
          desiredVelocities.get(SevenDoFArmJointEnum.elbowPitch).set(qDot);
       }
 
-//      {// Update current joint position and velocity for control 
-//         currentPositions.get(SevenDoFArmJointEnum.shoulderYaw).set(shoulderYawJoint.getQ());
-//         currentPositions.get(SevenDoFArmJointEnum.shoulderRoll).set(shoulderRollJoint.getQ());
-//         currentPositions.get(SevenDoFArmJointEnum.shoulderPitch).set(shoulderPitchJoint.getQ());
-//         currentPositions.get(SevenDoFArmJointEnum.elbowPitch).set(elbowPitchJoint.getQ());
-//         currentPositions.get(SevenDoFArmJointEnum.wristYaw).set(wristYawJoint.getQ());
-//         currentPositions.get(SevenDoFArmJointEnum.wristRoll).set(wristRollJoint.getQ());
-//         currentPositions.get(SevenDoFArmJointEnum.wristPitch).set(wristPitchJoint.getQ());
-//
-//         currentVelocities.get(SevenDoFArmJointEnum.shoulderYaw).set(shoulderYawJoint.getQd());
-//         currentVelocities.get(SevenDoFArmJointEnum.shoulderRoll).set(shoulderRollJoint.getQd());
-//         currentVelocities.get(SevenDoFArmJointEnum.shoulderPitch).set(shoulderPitchJoint.getQd());
-//         currentVelocities.get(SevenDoFArmJointEnum.elbowPitch).set(elbowPitchJoint.getQd());
-//         currentVelocities.get(SevenDoFArmJointEnum.wristYaw).set(wristYawJoint.getQd());
-//         currentVelocities.get(SevenDoFArmJointEnum.wristRoll).set(wristRollJoint.getQd());
-//         currentVelocities.get(SevenDoFArmJointEnum.wristPitch).set(wristPitchJoint.getQd());
-//      }
-
       // In the following, we perform the feedback control using simple PD
       // controllers.
       for (SevenDoFArmJointEnum jointEnum : SevenDoFArmJointEnum.values())
       {
          OneDoFJointReadOnly joint = robotJoints.get(jointEnum);
+         OneDoFJointStateBasics jointOutput = robotJointsOutputs.get(jointEnum);
+         
          // calculate desired effort based on current joint state
          double qError = desiredPositions.get(jointEnum).getValue() - joint.getQ();
          double qErrorDot = desiredVelocities.get(jointEnum).getValue() - joint.getQd();
@@ -206,9 +161,8 @@ public class RobotArmOneController implements Controller
          velocityErrors.get(jointEnum).set(qErrorDot);
 
          // Write desired effort as controller output
-         this.controllerOutput.getOneDoFJointOutput(jointEnum.getJointName()).setEffort(desiredEffort);
+         jointOutput.setEffort(desiredEffort);
       }
-
    }
 
    @Override
